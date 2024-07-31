4f5505b011974ca0d2eb9e8fdc5f964fba0878a8
2-TOUCH <ulyssesmorgan20020103@gmail.com>
2024-07-31 05:35:49 -0700

    Massive commit

--- run.py
+++ run.py
@@ -3,7 +3,14 @@ import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+Handler = CustomHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
